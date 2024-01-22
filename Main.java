76c8599c177aca820f4d5d18736af046ffdc3d3e
alexndrperz <alan80perez@gmail.com>
2024-01-22 18:26:08 -0400

    fix: del scanner

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         int iterator = 1;
         int cont = 0;
         do {
-            cont += iterator ;
+            cont = iterator + cont;
             iterator++;
             System.out.println(cont);
         } while (iterator < 20);
